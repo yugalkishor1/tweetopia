3d3bce1b42a50ee9d012c023619af3c28d67e3a0
Yugalkishor1 <yugalyadav755@gmail.com>
2024-03-22 23:36:48 +0530

    initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import Layout from "./laoyout/Layout.jsx"
+import Home from "./pages/Home.jsx"
+import Explore from "./pages/Explore.jsx"
+import "./index.css"
+
+const router = createBrowserRouter([{
+  path:"/",
+  element:<Layout/>,
+  children:[
+    {
+      index:true,
+      element:<Home/>
+    },
+    {
+      path:"explore",
+      element:<Explore/>
+    },
+  ]
+}])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
+
